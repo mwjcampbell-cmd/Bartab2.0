21dedbfa1939c936447477e1c8346911f864ece5
mwjcampbell-cmd <mwjcampbell@gmail.com>
2025-08-30 23:37:14 +1200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,14 @@
-const CACHE_NAME = 'bar-tab-cache-v1';
-const urlsToCache = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+const CACHE_NAME = "bartab-cache-v1";
+const urlsToCache = ["./","./index.html","./style.css","./script.js"];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>response||fetch(e.request))
   );
 });
